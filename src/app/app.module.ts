ab3846d1286018f7e47c6541c040838c5a3a14bd
Ovcharov Alex <hatekriss@gmail.com>
2020-11-18 17:01:15 +0300

    conditional loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { ApplicationRef, DoBootstrap, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { BrandFooterComponent } from './brand-footer/brand-footer.component';
 import { BrandHeaderComponent } from './brand-header/brand-header.component';
 
-const version = '1';
+const version = '3';
 console.log('(╯°□°)╯︵ ┻━┻: version', version);
 
 @NgModule({
@@ -21,10 +21,37 @@ console.log('(╯°□°)╯︵ ┻━┻: version', version);
   providers: [
     { provide: APP_BASE_HREF, useValue: environment.baseHref },
   ],
-  bootstrap: [
+  entryComponents: [
     AppComponent,
     BrandHeaderComponent,
-    BrandFooterComponent
-  ]
+    BrandFooterComponent,
+  ],
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+  ngDoBootstrap(app: ApplicationRef) {
+    if (/footer/gi.test(window.location.href)) {
+      this.bootstrapRootComponent(app, 'brand-footer');
+    }
+
+    if (/header/gi.test(window.location.href)) {
+      this.bootstrapRootComponent(app, 'brand-header');
+    }
+
+    if (/app/gi.test(window.location.href)) {
+      this.bootstrapRootComponent(app, 'app-root');
+    }
+  }
+
+  private bootstrapRootComponent(app: ApplicationRef, name: 'app-root' | 'brand-header' | 'brand-footer') {
+    // define the possible bootstrap components
+    // with their selectors (html host elements)
+    const options = {
+      'app-root': AppComponent,
+      'brand-header': BrandHeaderComponent,
+      'brand-footer': BrandFooterComponent
+    };
+
+    const component: any = options[name];
+    app.bootstrap(component);
+  }
+}
